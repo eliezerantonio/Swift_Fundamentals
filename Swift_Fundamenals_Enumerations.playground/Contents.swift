938de4c11fadcab6b3d462f96bc1226f2920b762
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-09-16 22:37:08 +0100

    feat(classes and structures): diference between classes and structures

--- Swift_Fundamenals_Enumerations.playground/Contents.swift
+++ Swift_Fundamenals_Enumerations.playground/Contents.swift
@@ -1,3 +1,139 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//----------------//
+// Enumerations //
+//--------------//
+// A structure that groups related values together
+// Type safe
+// case can have Int, String, Double, Float type
+
+enum Color {
+    case red
+    case green
+    case blue
+    case pink
+}
+
+var color = Color.green
+
+switch color {
+case .red:
+    print("this is a red color")
+case .green:
+    print("this is a gren color")
+default:
+    print("any other color")
+}
+
+// iteration over enum cases
+enum Meal: CaseIterable {
+    case chicken
+    case salad
+    case desert
+    case fish
+    case rice
+    case friendPotatoes
+    
+    func print() {
+        ///
+    }
+}
+
+var meal: Meal = .chicken
+
+for m in Meal.allCases {
+    print("m = \(m)")
+}
+
+
+// associated values
+enum Trip {
+    case Honolulu(Int)
+    case NewYork(Int)
+    case Tokyo(Int)
+    case Sydney(Int, String)
+    case Berlin
+    case London
+}
+
+let aTrip = Trip.Sydney(7, "Ritz")
+
+switch aTrip {
+case .Honolulu(let duration):
+    print("duration = \(duration)")
+case .Sydney(let duration, let hotel):
+    print("duration = \(duration), hotel = \(hotel)")
+default:
+    break
+}
+
+print("aTrip = \(aTrip)")
+
+// raw values
+// raw value can have Int, String, Float, Character
+enum Symbol: Character {
+    case a = "a"
+    case b = "b"
+    case q = "q"
+}
+
+let aSymbol = Symbol.q
+print("aSymbol = \(aSymbol)")
+
+if let newSymbol = Symbol(rawValue: "b") {
+    print("newSymbol = \(newSymbol)")
+}
+
+// implicit raw values
+enum Order: Int {
+    case one = 5
+    case two
+    case three
+    case four
+}
+
+let anOrder = Order.four
+print("anOrder = \(anOrder.rawValue)")
+
+enum Direction: String {
+    case left
+    case right
+    case straight
+    case down
+}
+
+//var direction = Direction.left
+var direction = Direction(rawValue: "down")
+print(direction?.rawValue ?? "")
+
+// recursive enumeration
+indirect enum ArithmeticCalculation {
+    case number(Int)
+    case add(ArithmeticCalculation, ArithmeticCalculation)
+    case multiply(ArithmeticCalculation, ArithmeticCalculation)
+}
+
+let seven = ArithmeticCalculation.number(7)
+let eight = ArithmeticCalculation.number(8)
+let ten = ArithmeticCalculation.number(10)
+
+// (10 + 7) * 8
+
+let sum = ArithmeticCalculation.add(ten, seven)
+let product = ArithmeticCalculation.multiply(sum, eight)
+
+let result = calculate(expression: product)
+print(result)
+
+func calculate(expression: ArithmeticCalculation) -> Int {
+    switch expression {
+    case .number(let value):
+        return value
+    case .add(let left, let right):
+        return calculate(expression: left) + calculate(expression: right)
+    case .multiply(let left, let right):
+        return calculate(expression: left) * calculate(expression: right)
+    }
+}
+
+
--- Swift_Fundamentals_Class_Struct.playground/Contents.swift
+++ Swift_Fundamentals_Class_Struct.playground/Contents.swift
@@ -1,3 +1,62 @@
 import UIKit
 
-var greeting = "Hello, playground"
+  //---------------------//
+ //  Structs & Classes  //
+//---------------------//
+
+struct Student {
+    let id: Int
+    var name: String
+    var address: String
+    func update() {
+        ///
+    }
+}
+
+class University {
+    let id: Int
+    var name: String
+    var students: [Student] = []
+    
+    init(id: Int,
+         name: String,
+         students: [Student]) {
+        self.id = id
+        self.name = name
+        self.students = students
+    }
+}
+
+var studentA = Student(id: 1,
+                       name: "Alice",
+                       address: "9001 Alaska St, Fresno, CA")
+
+var universityA = University(id: 1,
+                             name: "Concordia University",
+                             students: [studentA])
+
+print("student address = \(studentA.address)")
+print("university name = \(universityA.name)")
+
+studentA.update()
+
+studentA.name = "Bob"
+print("studentA name = \(studentA.name)")
+
+// value type, i.e. passed by value
+var studentACopy = studentA
+studentACopy.address = "10001 Townsend, San Francisco, CA"
+
+print("studentACopy address = \(studentACopy.address)")
+print("studentA address = \(studentA.address)")
+
+// pass by reference
+var universityARef = universityA
+universityARef.name = "CU"
+
+print("universityARef name = \(universityARef.name)")
+print("universityA name = \(universityARef.name)")
+
+if universityA === universityARef {
+    print("both universityA and universityARef refer to the same instance")
+}
